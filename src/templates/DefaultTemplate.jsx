95c1adc06855729fb21a178b483024aa99f41ce8
minsgy <alstjrchl97@naver.com>
2021-11-12 00:48:14 +0900

    🚩 refactor : DefaultTemplate 모바일 반응형 코드 삽입

--- src/templates/DefaultTemplate.jsx
+++ src/templates/DefaultTemplate.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import color from '@assets/colors/Color';
 import styled from 'styled-components';
+import { BREAKPOINT_PHONE, MediaQueries } from '@utils/MediaQuery';
 
 const MainWrapper = styled.div`
   display: flex;
-  height: 100vh;
   width: 100%;
-  box-sizing: border-box;
-  margin: 0 26px;
+  ${MediaQueries(BREAKPOINT_PHONE)} {
+    align-items: center;
+    flex-direction: column;
+    padding: 0 26px;
+  }
 `;
 
 const DefaultTemplate = ({ children }) => {
-  return <MainWrapper>{children}</MainWrapper>;
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  });
+
+  // 모바일 환경만 처리
+  return width < 456 ? (
+    <MainWrapper>{children}</MainWrapper>
+  ) : (
+    '태블릿/데스크탑은 지원하지 않습니다.'
+  );
 };
 
 export default DefaultTemplate;
