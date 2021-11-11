0a057079b7d86bdd4efaa2cc7289c43a83887840
minsgy <alstjrchl97@naver.com>
2021-11-11 16:59:18 +0900

    💄 style : font setting

---
+++ src/assets/fonts/Font.jsx
@@ -0,0 +1,18 @@
+import { css } from 'styled-components';
+
+export default {
+  Display1_Bold: css`
+    font-size: 18px;
+    font-weight: bold;
+  `,
+
+  Text1_Regular: css`
+    font-size: 10px;
+    font-weight: 400;
+  `,
+
+  Text2_Bold: css`
+    font-size: 14px;
+    font-weight: Bold;
+  `,
+};
