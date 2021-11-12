84d8c9b2b0cbf8565e38fd28955ce84e4456e223
minsgy <alstjrchl97@naver.com>
2021-11-12 21:00:56 +0900

    ✨ feat : 디테일 페이지 유저 프로필 컴포넌트 생성

---
+++ src/components/domain/UserDetail/ProfileImage.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ProfileImage = styled.img`
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+  border-radius: 15px;
+`;
+
+export default ProfileImage;
