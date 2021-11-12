1e00d6ebb70bb22af6e2985aaa6042241f2f2231
minsgy <alstjrchl97@naver.com>
2021-11-12 21:03:55 +0900

    ✨ feat : 임시로 상세 페이지 라우터 설정 완료

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,6 @@
 import React, { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { MainPage } from '@pages';
+import { UserDetailPage } from '@pages';
 import DefaultTemplate from '../templates/DefaultTemplate';
 const Router = () => {
   return (
@@ -7,6 +8,7 @@ const Router = () => {
       <Switch>
         <DefaultTemplate>
           <Route path="/" component={MainPage} exact />
+          <Route path="/:id" component={UserDetailPage} exact />
         </DefaultTemplate>
       </Switch>
     </BrowserRouter>
