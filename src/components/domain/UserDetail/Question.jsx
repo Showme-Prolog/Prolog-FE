30ac5d4f7b8937f2ff4dc3504c40674e1c63e04c
minsgy <alstjrchl97@naver.com>
2021-11-12 21:01:20 +0900

    ✨ feat : 질답 컴포넌트 분리 및 로직 적용 완료

---
+++ src/components/domain/UserDetail/Question.jsx
@@ -0,0 +1,45 @@
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
+
+import font from '@assets/fonts/Font';
+import color from '@assets/colors/Color';
+const QuestionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${color.white};
+`;
+
+const QuestionBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${color.white};
+  margin-bottom: 16px;
+`;
+
+const QuestionText = styled.h3`
+  ${font.Text2_Bold}
+  margin-bottom: 5px;
+`;
+
+const AnswerText = styled.p`
+  ${font.Text3_Regular}
+`;
+
+const Question = ({ question }) => {
+  const renderQuestion = useCallback(
+    (question) =>
+      question.map((item, index) => (
+        <QuestionBox key={index}>
+          <QuestionText>
+            Q{index + 1}. {item.Q}
+          </QuestionText>
+          <AnswerText>{item.A}</AnswerText>
+        </QuestionBox>
+      )),
+    [question],
+  );
+
+  return <QuestionWrapper>{renderQuestion(question)}</QuestionWrapper>;
+};
+
+export default Question;
