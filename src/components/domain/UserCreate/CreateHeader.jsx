f63ab90742fcf3a5f3ea94570ed52919404e66ea
minsgy <alstjrchl97@naver.com>
2021-11-13 17:27:14 +0900

    ✨ feat : CreateHeader 설정 완료 (후에 Header분리 리팩토링)

---
+++ src/components/domain/UserCreate/CreateHeader.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useHistory } from 'react-router';
+import font from '@assets/fonts/Font';
+import color from '@assets/colors/Color';
+import { ReactComponent as BackArrowIcon } from '@assets/icons/back-arrow.svg';
+import { CompleteButton } from '@components/common/Button';
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  box-sizing: border-box;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 53px;
+`;
+
+const HeaderItem = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: ${({ align }) => (align ? align : 'center')};
+  gap: 8px;
+`;
+
+const UsernameText = styled.h2`
+  ${font.Display1_Bold}
+  color: ${color.white};
+`;
+
+const DetailHeader = ({ username }) => {
+  const history = useHistory();
+
+  const handleBack = () => {
+    history.goBack();
+  };
+
+  return (
+    <HeaderWrapper>
+      <HeaderItem style={{ marginLeft: '9px' }} align="left">
+        <BackArrowIcon onClick={handleBack} />
+      </HeaderItem>
+      <HeaderItem>
+        <UsernameText align="center">{username}</UsernameText>
+      </HeaderItem>
+      <HeaderItem style={{ marginRight: '9px' }} align="right">
+        <CompleteButton text="완료" disabled />
+      </HeaderItem>
+    </HeaderWrapper>
+  );
+};
+
+export default DetailHeader;
