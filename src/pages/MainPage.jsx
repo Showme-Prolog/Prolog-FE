086c43cd6ef0674a0990e84196333b5254d494ec
minsgy <alstjrchl97@naver.com>
2021-11-16 21:31:42 +0900

    🧪 Test : response 데이터코드

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import UserContentList from '@components/domain/UserContentList';
 import MainHeader from '@components/domain/MainHeader';
+import { getIntroductionList } from '@api/IntroductionApi';
 
 const MainPage = () => {
+  const [userContentList, setUserContentList] = useState([]);
+
+  useEffect(async () => {
+    const response = await getIntroductionList();
+    console.log(response);
+    // const userContentList = await response.json();
+    // setUserContentList(userContentList);
+  }, []);
+
   return (
     <>
       <MainHeader />
