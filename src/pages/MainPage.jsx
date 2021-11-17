034d623c7d918e4630fba7ec6ef7f486a60fa464
minsgy <alstjrchl97@naver.com>
2021-11-18 01:38:42 +0900

    🧪 test : Test Link

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import UserContentList from '@components/domain/UserContentList';
 import MainHeader from '@components/domain/MainHeader';
@@ -20,6 +21,8 @@ const MainPage = () => {
     <>
       <MainHeader />
       <UserContentList />
+      <Link to="/question" />
+      <Link to="/questiondetail" />
     </>
   );
 };
